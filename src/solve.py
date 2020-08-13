30b104455ee6d671eee755d32e0b5e1626115c77
Ferry <alessandro.ferrari.2004@gmail.com>
2020-08-13 10:47:49 -0400

    removed depth for noew

--- src/solve.py
+++ src/solve.py
@@ -81,7 +81,7 @@ def bfs(dominoes, maxFrontierSize, maxDepth, verbose, dominoesNumber):
             childNode = Node(node, dominoes[i])
             if childNode.state not in explored:
                 if childNode.state.isASolution():
-                    print("Found A Solution! Depth: " + str(depth))
+                    print("Found A Solution!")
                     for i in range(0, depth):
                         print(childNode.addedDomino)
                         childNode = childNode.parentNode
